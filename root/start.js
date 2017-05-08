996803e03482eaa738bd82afeda9d1ac565e14eb
FezZHD <zhenya535@gmail.com>
2017-05-09 02:38:36 +0300

    default server implementation

--- root/start.js
+++ root/start.js
@@ -7,15 +7,31 @@
 
 var express = require("express");
 
+var WebSocket = require("ws").Server;
+
+var mysql = require("mysql");
 
 var app = express();
 
+var notificationSocket = new WebSocket({port:3031, path: "/notificationSocket"});
 
-app.get("/", function (req, res) {
+var router  = express.Router();
+
+notificationSocket.on("connection", function connection(ws){
+
+    console.log("connected");
+
+});
+
+router.get("/", function (req, res) {
     res.status(200);
     res.send("hello");
 } );
 
+
+router.post("/register", function (req, res) {
+
+});
 
 app.listen(3030, function () {
     console.log("server start");
