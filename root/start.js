bde176d3650f8eb61ccb48e3df04a31725869319
FezZHD <zhenya535@gmail.com>
2017-05-09 20:56:56 +0300

    implementing post registration

--- root/start.js
+++ root/start.js
@@ -9,28 +9,71 @@ var express = require("express");
 
 var WebSocket = require("ws").Server;
 
+var bodyParser = require("body-parser")
 var mysql = require("mysql");
 
 var app = express();
 
+
+var db = mysql.createConnection({
+    host:"127.0.0.1",
+    port:3306,
+    user:"root",
+    password:"1234",
+    database:"notishare"
+});
+
+db.connect(function (err) {
+    console.log(err);
+});
 var notificationSocket = new WebSocket({port:3031, path: "/notificationSocket"});
 
 var router  = express.Router();
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+var wsArray = {};
 notificationSocket.on("connection", function connection(ws){
 
-    console.log("connected");
+    ws.on("message", function incoming(message) {
 
+    });
+    ws.on("close", function close() {
+        
+    });
+    console.log("connected");
+    ws.send("Hello");
 });
 
-router.get("/", function (req, res) {
+app.get("/", function (req, res) {
     res.status(200);
     res.send("hello");
 } );
 
 
-router.post("/register", function (req, res) {
+app.post("/register", function (req, res) {
 
+    var body = req.body;
+    var selectQuery = db.query("SELECT userName FROM user WHERE userName = ?", body.userName, function (error, rows, fields) {
+        console.log(error);
+        if (rows.length === 0){
+            var query = db.query("INSERT INTO user SET ?", body, function (error, result) {
+                console.log(error);
+                if (error === null){
+                    res.status(200);
+                    res.send("registered");
+                }
+            });
+        }else {
+            res.status(401)
+            res.send("User already exist");
+        }
+    })
 });
 
 app.listen(3030, function () {
