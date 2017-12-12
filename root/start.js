b509b76617a3bb10d64fac1aa512c0096cc2674c
FezZHD <fezzdev@gmail.com>
2017-12-12 05:01:25 +0300

    fixing server insert to db

--- root/start.js
+++ root/start.js
@@ -55,20 +55,21 @@ notificationSocket.on("connection", function connection(ws){
             if (error){
                 throw error;
             }
-
-            let query = db.query("INSERT INTO notification_data SET ?",[notificationObject.title, notificationObject.notificationText, notificationObject.image, currentSocketArray.user_device_id] , function (error, result) {
+            let notification_data = {notification_title: notificationObject.notification_title, notification_text: notificationObject.notification_text, image:  notificationObject.image, user_device_id:currentSocketArray.user_device_id,datetime_sending:notificationObject.datetime_sending  };
+            let query = db.query("INSERT INTO notification_data SET ?", notification_data , function (error, result) {
                 throwDbError(error)
+                db.commit(function (error) {
+
+                    if (error){
+                        db.rollback(function () {
+                            console.log(error);
+                        })
+                    }
+                })
             });
         });
 
-        db.commit(function (error) {
 
-            if (error){
-                db.rollback(function () {
-                    console.log(error);
-                })
-            }
-        })
 
     });
 
@@ -105,20 +106,21 @@ clipboardSocket.on("connection", function connection(ws){
             if (error){
                 throw error;
             }
+            let insert_clipboard = {clipboardText: clipboardObject.clipboardText, clipboardDateCreation: clipboardObject.clipboardDateCreation, user_device_id: user_deviceId,clipboard_data_type_id: clipboardObject.clipboard_data_type_id  };
+            let query = db.query("INSERT INTO clipboard SET ?", insert_clipboard , function (error, result) {
+                throwDbError(error);
+                db.commit(function (error) {
 
-            let query = db.query("INSERT INTO clipboard SET ?",[clipboardObject.clipboardData, clipboardObject.datetimeCreation, user_deviceId, clipboardObject.dataType] , function (error, result) {
-                throwDbError(error)
+                    if (error){
+                        db.rollback(function () {
+                            console.log(error);
+                        })
+                    }
+                })
             });
         });
 
-        db.commit(function (error) {
 
-            if (error){
-                db.rollback(function () {
-                    console.log(error);
-                })
-            }
-        })
     });
 
     ws.on("close", function close() {
@@ -183,48 +185,62 @@ app.get("/", function (req, res) {
 
 app.post("/register", function (req, res) {
     let body = req.body;
-    let selectQuery = db.query("SELECT DISTINCT user.id FROM user JOIN user_auth ON user_auth.userName = ? AND user.id = user_auth.id", body.userName, function (error, rows, fields) {
-        console.log(error);
+    let user_name = body.user_name;
+    let selectQuery = db.query("SELECT DISTINCT user.id FROM user JOIN user_auth ON user_auth.user_name =? AND user.id = user_auth.id", user_name, function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+            throw error;
+        }
         if (rows.length === 0){
-
-            db.beginTransaction(function (error) {
-                if (error){
-                    throw error;
-                }
-                let quetyAuth = db.query("INSERT INTO user_auth SET ?", [body.userName, body.passwordHash], function (error, result) {
-                    throwDbError(error)
-
-                    let userAuthId = result.insertId;
-
-                    let queryProfile = db.query("INSERT INTO profile SET ?", [body.name, body.surname, body.email], function (error, result) {
-
-                            throwDbError(error);
-
-
-
-                            let profileId = result.insertId;
-
-
-                            let queryAll = db.query("INSERT INTO user SET ?" , [userAuthId, profileId], function (error, result) {
-                                throwDbError(error);
-                            });
-                    });
-
-                })
-
-            });
-
-            db.commit(function (error) {
-
-                if (error){
-                    db.rollback(function () {
-                        putResponse(500, "Transaction error", res);
+            try {
+                db.beginTransaction(function (error) {
+                    if (error) {
                         console.log(error);
+                        throw error;
+                    }
+                    let user_auth_data = {user_name: user_name, password_hash: body.password_hash};
+                    let quetyAuth = db.query("INSERT INTO user_auth SET ?", user_auth_data, function (error, result) {
+                        throwDbWithResponse(error, res, "Transaction error");
+                        if (error) {
+                            return;
+                        }
+                        let user_auth_id = result.insertId;
+                        let profileData = {name: body.name, surname: body.surname, email: body.email};
+                        let queryProfile = db.query("INSERT INTO profile SET ?", profileData, function (error, result) {
+
+                            throwDbWithResponse(error, res, "Transaction error");
+                            if (error) {
+                                return;
+                            }
+
+
+                            let user_data = {user_auth_id: user_auth_id, profile_id: result.insertId};
+                            let queryAll = db.query("INSERT INTO user SET ?", user_data, function (error, result) {
+                                throwDbWithResponse(error, res, "Transaction error");
+                                db.commit(function (error) {
+
+                                    if (error) {
+                                        db.rollback(function () {
+                                            putResponse(500, "Transaction error", res);
+                                            console.log(error);
+                                            throw error;
+                                        });
+                                        return;
+                                    }
+                                    putResponse(200, "Registered", res);
+                                });
+                            });
+                        });
+
                     });
-                    return;
-                }
-                putResponse(200, "Registered", res);
-            });
+
+                });
+
+
+            }
+        catch (error){
+            console.log(error);
+        }
         }else {
             putResponse(401, "User already exist", res);
         }
@@ -234,7 +250,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     let body = req.body;
-    let query = db.query("SELECT DISTINCT user.id FROM user JOIN user_auth ON user_auth.user_name =? AND user_auth.password_hash =? AND user_auth.id = user.user_auth_id", [body.userName, body.passwordHash], function (error, rows, fields) {
+    let query = db.query("SELECT DISTINCT user.id FROM user JOIN user_auth ON user_auth.user_name =? AND user_auth.password_hash =? AND user_auth.id = user.user_auth_id", [body.user_name, body.password_hash], function (error, rows, fields) {
         if (rows.length !== 0){
             let objectToSend = new function () {
                 this.userId = rows[0].id;
@@ -242,18 +258,32 @@ app.post("/login", function (req, res) {
             };
             putResponse(200, JSON.stringify(objectToSend), res);
         }else {
-            putResponse(401, "Error. Incorrect auth data", res);
+            let objectToSend = new function () {
+                this.userId = -1;
+                this.message = "Error. Incorrect auth data";
+            };
+            putResponse(401, JSON.stringify(objectToSend), res);
         }
     })
 });
 
 
 
+function throwDbWithResponse(error, res, message) {
+    if (error){
+        console.log(error);
+        db.rollback(function () {
+            putResponse(500, message, res);
+            throw error;
+        })
+    }
+}
+
 function throwDbError(error) {
     if (error){
         console.log(error);
         db.rollback(function () {
-            throw error;
+
         })
     }
 }
@@ -269,57 +299,90 @@ app.post("/registerDevice", function (req, res) {
         this.message = null;
     };
 
+    let error_object_to_send = {idUserDevice: -1, message: "Transaction error"};
+    var existed_device = null;
 
     var user_device_id = null;
 
-    let selectDeviceQuery = db.query("SELECT DISTINCT id FROM user_device JOIN device ON user_device.device_id = device.id AND user_auth.user_id=? JOIN device_info ON device_info.deviceId=?", [body.userId, body.deviceId], function (error, rows, field) {
+    let selectDeviceQuery = db.query("SELECT DISTINCT user_device.id FROM user_device JOIN device ON user_device.device_id = device.id AND user_device.user_id=? JOIN device_info ON device_info.deviceId=?", [body.user_id, body.deviceId], function (error, rows, field) {
 
         if (error){
             console.log(error);
+            throw error;
         }
 
-        if (rows.length === 0){
-            //create device at db
 
-            db.beginTransaction(function (error) {
+
+        if (rows.length === 0) {
+            //create device at db
+            let select_device_query = db.query("SELECT DISTINCT device.id FROM device JOIN device_info ON device.device_info_id = device_info.iddevice_info AND device_info.deviceId=? ", body.deviceId, function (error, rows, field) {
+
                 if (error){
                     console.log(error);
-                    throw error;
+                }else{
+
+                    if (rows.length !== 0) {
+                        existed_device = rows[0].id;
+                    }
                 }
-
-
-                let insertDeviceInfo = db.query("INSERT INTO device_info SET ?", [body.deviceId, body.deviceName], function (error, result) {
-
-                    throwDbError(error);
-
-
-                    let insertDevice = db.query("INSERT INTO device SET ?", [result.insertId, body.deviceType], function (error, result) {
-                        throwDbError(error);
-
-                        let insertUserDevice = db.query("INSERT INTO user_device SET ?", [body.userId, result.insertId], function (error, result) {
-
-                            throwDbError(error);
-
-                            //todo send user_device_id
-                            user_device_id = result.insertId;
-                        })
-                    })
-                })
             });
+            try {
+                db.beginTransaction(function (error) {
+                    if (error) {
+                        console.log(error);
+                        throw error;
+                    }
+                            if (existed_device === null) {
+                                let device_info_data = {deviceId: body.deviceId, device_name: body.device_name};
+                                let insertDeviceInfo = db.query("INSERT INTO device_info SET ?", device_info_data, function (error, result) {
+
+                                    throwDbWithResponse(error, res, JSON.stringify(error_object_to_send));
+                                    if (error) {
+                                        return;
+                                    }
 
 
-            db.commit(function (error) {
+                                    let device_data = {device_info_id: result.insertId, device_type_id: body.device_type_id};
+                                    let insertDevice = db.query("INSERT INTO device SET ?", device_data, function (error, result) {
+                                        throwDbWithResponse(error, res, JSON.stringify(error_object_to_send));
 
-                if (error){
-                   db.rollback(function () {
-                       putResponse(500, "Bad request", res);
-                       console.log(error)
-                   });
-                }
-                objectToSend.idUserDevice = user_device_id;
-                objectToSend.message = "Device registered";
-                putResponse(200, JSON.stringify(objectToSend), res);
-            })
+                                        if (error) {
+                                            return;
+                                        }
+                                        existed_device = result.insertId;
+                                    });
+                                });
+                            }
+                            let user_device_data = {device_id: existed_device, user_id: body.user_id};
+                            let insertUserDevice = db.query("INSERT INTO user_device SET ?", user_device_data, function (error, result) {
+
+                                throwDbWithResponse(error, res, JSON.stringify(error_object_to_send));
+                                if (error) {
+                                    return;
+                                }
+                                //todo send user_device_id
+                                user_device_id = result.insertId;
+                            });
+                            db.commit(function (error) {
+
+                                if (error) {
+                                    db.rollback(function () {
+                                        putResponse(500, JSON.stringify(objectToSend), JSON.stringify(error_object_to_send));
+                                        console.log(error);
+                                        throw error;
+                                    });
+                                    return;
+                                }
+                                objectToSend.idUserDevice = user_device_id;
+                                objectToSend.message = "Device registered";
+                                putResponse(200, JSON.stringify(objectToSend), res);
+                            });
+
+                        })
+
+            }catch(error){
+                console.log(error);
+            }
         }
         else {
             objectToSend.idUserDevice = rows[0].id;
@@ -342,6 +405,9 @@ function putResponse(code, message, res) {
 app.listen(3030, function () {
     console.log("server start");
 });
+
+
+
 
 
 
